a14e03b29e5039482fb8a32e6c866dd3125432ad
Anna Whitney <awhitney@college.harvard.edu>
2015-08-20 22:15:27 -0400

    update database file to include new tables

--- wheretolive.sql
+++ wheretolive.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2015 at 06:01 PM
+-- Generation Time: Aug 20, 2015 at 10:14 PM
 -- Server version: 5.5.41-0ubuntu0.14.04.1-log
 -- PHP Version: 5.5.9-1ubuntu4.6
 
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `wheretolive`
 --
+CREATE DATABASE IF NOT EXISTS `wheretolive` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `wheretolive`;
 
 -- --------------------------------------------------------
 
@@ -37,22 +39,7 @@ CREATE TABLE IF NOT EXISTS `cities` (
   `population` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `city_name` (`name`,`state`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=11 ;
-
---
--- Dumping data for table `cities`
---
-
-INSERT INTO `cities` (`id`, `name`, `state`, `rent`, `walkscore`, `transitscore`, `bikescore`, `population`) VALUES
-(1, 'Berkeley', 'CA', 1253, 79, 56, 89, 117000),
-(2, 'Kansas City', 'MO', 723, 32, 34, 40, 467000),
-(3, 'Austin', 'TX', 950, 35, 33, 52, 885000),
-(4, 'Denver', 'CO', 863, 56, 47, 71, 650000),
-(5, 'Boulder', 'CO', 1132, 56, 49, 86, 103000),
-(7, 'Seattle', 'WA', 1051, 71, 57, 63, 652000),
-(8, 'Portland', 'OR', 885, 63, 50, 72, 609000),
-(9, 'Nashville', 'TN', 816, 26, 0, 33, 658000),
-(10, 'Asheville', 'NC', 811, 35, 0, 0, 87000);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=30 ;
 
 -- --------------------------------------------------------
 
@@ -71,20 +58,6 @@ CREATE TABLE IF NOT EXISTS `inprogress` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=9 ;
 
---
--- Dumping data for table `inprogress`
---
-
-INSERT INTO `inprogress` (`id`, `user`, `salary`, `status`, `notes`, `company`, `position`) VALUES
-(1, 1, 24680, '', 'will this reappear?', 'Add City Test', 'Tester'),
-(2, 1, 29478, '', 'see if this reappears', 'Hi', 'Yeah'),
-(3, 1, 29478, '', 'see if this reappears', 'Hi', 'Yeah'),
-(4, 1, 90358, '', 'see if this reappears', 'blah', 'aldkjfa'),
-(5, 1, 979897988, 'interested', 'Enter notes here yay', 'Hi', 'yo'),
-(6, 1, 902580, 'applied', 'Woo gonn Enter some notes here', 'lajgdklfa', 'akdslfja'),
-(7, 1, 24870, 'interested', '', 'lajf', 'alkdjflksd'),
-(8, 1, 98765, 'offer', 'Notes me bro', 'alkfjskla', 'hi');
-
 -- --------------------------------------------------------
 
 --
@@ -101,18 +74,19 @@ CREATE TABLE IF NOT EXISTS `jobs` (
   `company` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `position` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=10 ;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `jobs`
+-- Table structure for table `sharing`
 --
 
-INSERT INTO `jobs` (`id`, `user`, `city`, `salary`, `status`, `notes`, `company`, `position`) VALUES
-(1, 1, 1, 123456, 'applied', 'testing testing TEST 10840239840293 yo notes are cool my hands are typing words aljdf;lkajds;lakjf;lskaj;fa AKHFJAWIEJAKDJFLK;DSFJA;', 'Testing', 'Tester'),
-(2, 1, 1, 54321, '', 'YEAHHHHHHHHHHHHH', 'More hi', 'more what?'),
-(3, 1, 4, 39871, 'interested', 'see if this reappears', 'blah', 'aldkjfa'),
-(4, 1, 7, 0, 'applied', 'Woo gonn Enter some notes here', 'lajgdklfa', 'akdslfja'),
-(5, 1, 9, 98765, 'offer', 'Notes me bro', 'alkfjskla', 'hi');
+CREATE TABLE IF NOT EXISTS `sharing` (
+  `user` int(11) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`user`,`shared`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -128,21 +102,6 @@ CREATE TABLE IF NOT EXISTS `usercities` (
   PRIMARY KEY (`user`,`city`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `usercities`
---
-
-INSERT INTO `usercities` (`user`, `city`, `rank`, `notes`) VALUES
-(1, 1, 1, ''),
-(1, 2, 3, ''),
-(1, 3, 7, ''),
-(1, 4, 6, ''),
-(1, 5, 4, ''),
-(1, 7, 5, ''),
-(1, 8, 2, ''),
-(1, 9, 8, ''),
-(1, 10, 9, '');
-
 -- --------------------------------------------------------
 
 --
@@ -156,14 +115,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `numcities` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `hash`, `numcities`) VALUES
-(1, 'anna', '$1$ILn.wIqw$G9AdhR3NCg1uPli.079Ji/', 9);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=4 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
